1a2793fa7b382393b7ce3a9d12ef403a46f26678
fatima pervaiz <fatimahpervaiz95@gmail.com>
2024-09-26 21:05:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let score =0
+let ques1=prompt("what is your name?");
+if (ques1==="fatima"){
+
+    score++ 
+}
+let ques2=prompt("How many peoples in the world?");
+if(ques2==="7.951"){
+    score++
+}
+let ques3=prompt("How many fingers in hands?");
+     if (ques3==="10"){
+    score++
+}
+let ques4=prompt("what is the color of parrot?");
+ if (ques4==="green"){
+    score++
+}
+alert("your total score   "+score)
